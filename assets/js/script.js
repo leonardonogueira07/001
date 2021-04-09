29ca320b6e1702f3393e0a00b708a05a80ae511f
Leonardo Nogueira <leojunio007@gmail.com>
2021-04-08 21:19:39 -0300

    Valores - Mascara

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,121 +17,7 @@ btn1.addEventListener('click', function() {
     } else {
         container.style.display = 'none';
     }
-});
-
-/*FORMULARIO
-function transac() {
-    if (document.querySelector("input").value==''){ 
-        alert('Preencha o campo de mercadoria');       
-        document.querySelector("input").focus();
-                
-    } else {
-        let tipo = document.getElementById('selecao').value;
-        let mercadoria = document.getElementById('mercadoria').value;
-        let valor = document.getElementById('valor').value;
-        console.log(transac);
-        
-        let transacValor = {
-            nameSelecao: tipo,
-            nameMercadoria: mercadoria,
-            nameValor: valor        
-        };console.log(transacValor);
-    }
-       
-};*/
-/*
-    let button = document.querySelector('button');
-    let mercadoria = document.getElementById('mercadoria');
-    let valor = document.getElementById('valor');    
-    let para = document.getElementById('msg');
-    let contador = 0;
-    button.onclick = function(e) {
-        if (mercadoria.value || valor.value ){            
-            para.textContent = '';
-            contador += 1;
-            console.log("Deu Ruim!");
-        } else {
-            para.textContent = 'Voçê prescisa inserir algo nos campos acima!';
-            contador += 1;
-            console.log("Deu Bom demais");
-        };
-        if (contador > 0) {
-
-            e.preventDefault();
-
-        }
-        
-    } */
-/*
-    let selecao = document.getElementById('selecao');
-    let mercadoria = document.getElementById('mercadoria');
-    let valor = document.getElementById('valor');    
-
-    function validation() {        
-
-        if(mercadoria.value == "" || mercadoria.value.length < 3) {
-            document.getElementById("msgMercadoria").textContent = "Preencha este campo!";
-            mercadoria.focus();
-            return false
-        } else {
-            document.getElementById("msgMercadoria").textContent = "";
-            
-        }
-        if(valor.value == "" || valor.length < 2) {
-            document.getElementById("msgValor").textContent  = "Preencha este campo!";
-            valor.focus();
-            return false
-        } else {
-            document.getElementById("msgValor").textContent = "";
-            
-        }
-        
-    }*/
-
-
-/* atualizada
-    function validation () {
-        let Vselecao = document.getElementById('selecao').value;
-        let Vmercadoria = document.getElementById('mercadoria').value;
-        let Vvalor = document.getElementById('valor').value; 
-
-        let conteudo = {
-            selecao : Vselecao,
-            mercadoria: Vmercadoria,
-            valor : Vvalor
-        };
-        
-        if(Vmercadoria == "" || Vmercadoria.length < 3) {
-            document.getElementById("msgMercadoria").textContent = "Preencha este campo!";
-            
-            return false
-        } else {
-            document.getElementById("msgMercadoria").textContent = "";
-            
-        }
-        if(Vvalor == "" || Vvalor.length < 2) {
-            document.getElementById("msgValor").textContent  = "Preencha este campo!";
-            
-            return false
-        } else {
-            document.getElementById("msgValor").textContent = "";
-            
-        }
-        
-
-        console.log(conteudo);
-    } 
-    
-
-    function cadastrar(Vselecao, Vmercadoria, Vvalor ) {
-        let novoCad = {selecao: Vselecao, mercadori: Vmercadoria, valor : Vvalor };
-
-        if(typeof(Storage) !== "undefined") {
-
-        }
-        else alert("dsad");
-    } */
-
+}); 
     //FORMULARIO VALIDAÇÃO = MESSAGE PERSONALIZADA
     const campos = document.querySelectorAll("[required]")
 
@@ -158,7 +44,7 @@ function transac() {
                 text: {
                     valueMissing: "Por favor, preencha a mercadoria"
                 },
-                number: {
+                text: {
                     valueMissing: "Faltou informar o valor"
                 }                               
             }
@@ -203,6 +89,29 @@ function transac() {
         })
         campo.addEventListener("blur", validacao)
     }
+    //SOMENTE NUMEROS
+    function somenteNum(evt) {
+        var charCode = (evt.which) ? evt.which : Event.charCode
+        if(charCode > 31 && (charCode < 48 || charCode > 57))
+
+        return false;
+        return true;
+
+    }//MASCARA DE NUMEROS
+    function mascaraValor(str) {
+        var v = str.value.replace(/\D/g,'')
+        
+        v = (v/100).toFixed(2) + ''
+        v = v.replace(".", ",")
+        v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,")
+        v = v.replace(/(\d)(\d{3}),/g, "$1.$2,")
+
+        str.value = v
+    }
+
+
+
+
 //EXTRATO DE TRANSAÇÕES
     //EXTRATO DE TRANSAÇÕES
     function dados() {
